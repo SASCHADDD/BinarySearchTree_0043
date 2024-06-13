b80e9f0c7b93971fca4244a31b4ff4c11ab144af
SASCHADDD <saschadanu23@gmail.com>
2024-06-13 18:30:04 +0700

    memindahkan memory ke new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -28,4 +28,7 @@ class BinaryTree
             ROOT = nullptr; // initializing root to null
         }
     void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element,nullptr, nullptr); //allocate memory for the new node
+    }
 }
